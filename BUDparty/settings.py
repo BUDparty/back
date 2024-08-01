1e6fbc14dd5e0aa012b227952cf45cea804d6d27
LSY <tmdduf54@gachon.ac.kr>
2024-08-01 16:45:28 +0900

    타입캐스트

--- BUDparty/settings.py
+++ BUDparty/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(BASE_DIR, 'path_to_your_google_cloud_credentials.json')
 load_dotenv(os.path.join(BASE_DIR, '.env'))
 API_KEY = os.getenv('API_KEY')
-TYPECAST_API_KEY = os.getenv('TYPECAST_API_KEY')
+TYPECAST_API_KEY = os.getenv('T_API_KEY')
 
 # Logging configuration
 logging.basicConfig(level=logging.DEBUG)
